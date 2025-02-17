f25f310ec40e71b72b3cefe90f23c324629f38e7
c2csabal <c2csabal@vasvari.org>
2025-02-17 09:07:58 +0100

    Vásárlás nélküli napok

--- vasarlas.py
+++ vasarlas.py
@@ -1,21 +1,20 @@
 f = open('vasarlas.csv','r', encoding='utf-8')
-
-napokSzama = 0
-vasarlasNelkuliNapok = 0
 sor = (f.readline())
-szamok = []
 sor = sor.strip().split(';')
-for szam in sor:
-    szamok.append(int(szam))
-    napokSzama += 1
-    if szam == 0:
-        vasarlasNelkuliNapok += 1
-
 f.close
+
+def vasarlasNelkuliNapok(sorok):
+    napokSzama = 0
+    vasarlasNelkuliNapok = 0
+    szamok = []
+    for szam in sorok:
+        szamok.append(int(szam))
+        napokSzama += 1
+        if szam == '0':
+            vasarlasNelkuliNapok += 1
+    return vasarlasNelkuliNapok
+    
+VNN = vasarlasNelkuliNapok(sor)
+
 print(sor)
-print("Átlag: ",sum(szamok)/len(szamok))
-print("Legkisebb szám: ",min(szamok))
-print("Legnagyobb szám: ",max(szamok))
-print("Összes költés: ",sum(szamok))
-print(vasarlasNelkuliNapok,"nap nem költött semmit.")
-print(napokSzama,"napos volt a hónap")
+print(VNN,"nap nem költött semmit.")
