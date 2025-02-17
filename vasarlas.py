38e24e129a0c58cb11cdfef134ae5cdbd1ee3ad0
c2csabal <c2csabal@vasvari.org>
2025-02-17 09:18:39 +0100

    Számbeolvasás

--- vasarlas.py
+++ vasarlas.py
@@ -3,18 +3,26 @@ sor = (f.readline())
 sor = sor.strip().split(';')
 f.close
 
-def vasarlasNelkuliNapok(sorok):
-    napokSzama = 0
-    vasarlasNelkuliNapok = 0
+def Szambeolv(sorok):
     szamok = []
     for szam in sorok:
         szamok.append(int(szam))
+    return szamok
+
+def vasarlasNelkuliNapok(sorok):
+    napokSzama = 0
+    vasarlasNelkuliNapok = 0
+    for szam in sorok:
         napokSzama += 1
         if szam == '0':
             vasarlasNelkuliNapok += 1
     return vasarlasNelkuliNapok
-    
+szamok = Szambeolv(sor)
 VNN = vasarlasNelkuliNapok(sor)
 
 print(sor)
 print(VNN,"nap nem költött semmit.")
+print("Átlag: ",sum(szamok)/len(szamok))
+print("Legkisebb szám: ",min(szamok))
+print("Legnagyobb szám: ",max(szamok))
+print("Összes költés: ",sum(szamok))
