f9b454d0d15bd6b3cf717e6109fafe41272f64b7
c2csabal <c2csabal@vasvari.org>
2025-02-17 09:32:34 +0100

    Egymás utanáni nap költés nélkül

--- vasarlas.py
+++ vasarlas.py
@@ -17,12 +17,28 @@ def vasarlasNelkuliNapok(sorok):
         if szam == '0':
             vasarlasNelkuliNapok += 1
     return vasarlasNelkuliNapok
+
+def egymasUtanKoltesNelkuliNap(sorok):
+    KoltesNelkuliNapokSoroatba = 1
+    KoltesNelkuliNapokSoroatbaOsszes = 0
+    for i in range(len(sorok)):
+        nap = sorok[i]
+        if nap == '0':
+            if nap == sorok[i-1]:
+                KoltesNelkuliNapokSoroatba += 1
+                if KoltesNelkuliNapokSoroatbaOsszes < KoltesNelkuliNapokSoroatba:
+                    KoltesNelkuliNapokSoroatbaOsszes = KoltesNelkuliNapokSoroatba 
+            else:
+                KoltesNelkuliNapokSoroatba = 1
+    return KoltesNelkuliNapokSoroatbaOsszes
+
 szamok = Szambeolv(sor)
 VNN = vasarlasNelkuliNapok(sor)
-
+KNNS = egymasUtanKoltesNelkuliNap(sor)
 print(sor)
 print(VNN,"nap nem költött semmit.")
 print("Átlag: ",sum(szamok)/len(szamok))
 print("Legkisebb szám: ",min(szamok))
 print("Legnagyobb szám: ",max(szamok))
 print("Összes költés: ",sum(szamok))
+print(KNNS, "napig sorozatba nem költött semmit.")
